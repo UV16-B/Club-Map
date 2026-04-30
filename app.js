149b160fdfb317da3d2e6d2befd33cd28f74fb37
UV16-B <natusya477@gmail.com>
2026-04-30 22:13:01 +1000

    Приватизация аккаунта админа

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express'); const fs = require('fs'); const path = requi
 const app = express();
 app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/admin', (req, res) => res.sendFile(path.join(__dirname, 'views', 'admin.html')));
 app.get('/api/cities', (req, res) => {
     fs.readFile('./data/cities.json', 'utf8', (err, data) => {
